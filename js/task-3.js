ee879f978800288ef5cf188e9417b79a26b34de6
ozncncyr <ozncncyr@outlook.com>
2025-01-24 02:42:05 +0300

    Fix syntax error in getInfo method of player profile object

--- js/task-3.js
+++ js/task-3.js
@@ -11,7 +11,7 @@ const profile = {
     },
     getInfo() {
         return `${this.username} has ${this.playTime} active hours!`
-    }
+    },
 };
 
 console.log(profile.getInfo()); // "Jacob has 300 active hours!"
